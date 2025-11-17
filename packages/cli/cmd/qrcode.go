6778083bc835e707dd15c21bb05cd9c2ab9a9040
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-17 17:31:35 +0700

    update cli

--- packages/cli/cmd/qrcode.go
+++ packages/cli/cmd/qrcode.go
@@ -3,7 +3,7 @@ package cmd
 
 import (
 	"fmt"
-	"ig-cli/utils"
+	"ig-cli/utils/figlet"
 	"os"
 	"path/filepath"
 
@@ -23,7 +23,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		utils.LogProgramName()
+		figlet.LogProgramName()
 		// Get URL
 		fmt.Print("URL: ")
 		var url string
